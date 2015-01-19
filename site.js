a55373ae9e10f532ab21a24a3a32b2e2e4e44b4b
samanpwbb <samanpwbb@gmail.com>
2015-01-19 17:49:56 -0500

    basic form template

--- site.js
+++ site.js
@@ -16,10 +16,27 @@ $(document).ready(function() {
 
   /* Create form */
   var form = _.template($('.js-template').html());
-  $('.js-form').html(form());
+  $('.js-configurable').on('click', function(ev) {
 
-  var pelvis = document.getElementById('pelvis');
+    $('.js-form').html(form({element: ev.currentTarget.id}));
 
+    var bodyPart = ev.currentTarget;
+
+    /* Event handlers */
+    $('.js-height-up').on('click', function(ev) {
+      changeClass(bodyPart, true, 'height');
+      return false;
+    });
+
+    $('.js-height-down').on('click', function(ev) {
+      changeClass(bodyPart, false, 'height');
+      return false;
+    });
+
+  });
+
+
+  /* Manipulate attributes */
   var getClassFromList = function(el, classRegex) {
     return el.className.match(classRegex)[0];
   };
@@ -34,23 +51,19 @@ $(document).ready(function() {
     var currentNum = getNumberFromClass(currentClass);
     var newNum = currentNum + (increment ? 1 : -1);
 
-    if (currentNum >= config[attribute].max) {
-      newNum = config[attribute].min;
+    if (increment) {
+      if (currentNum >= config[attribute].max) {
+        newNum = config[attribute].min;
+      }
+    } else {
+      if (currentNum <= config[attribute].min) {
+        newNum = config[attribute].max;
+      }
     }
 
     var newClass = 'h' + newNum;
 
     $(el).removeClass(currentClass).addClass(newClass);
   };
-
-  $('.js-height-up').on('click', function(ev) {
-    changeClass(pelvis, true, 'height');
-    return false;
-  });
-
-  $('.js-height-down').on('click', function(ev) {
-    changeClass(pelvis, false, 'height');
-    return false;
-  });
 
 });
