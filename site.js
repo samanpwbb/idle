2d735ebfd004331151e32ed6f3c869e14ed28d27
samanpwbb <samanpwbb@gmail.com>
2015-01-29 07:33:13 +0100

    fix everything, show active state

--- site.js
+++ site.js
@@ -1,73 +1,66 @@
+/* TODO
+1a. Dismiss form when user clicks off
+2. Highlight active settings
+3. Design a bunch of special parts (with special[1-9] + class toggle + psuedo elements)
+4. Let user save? URL parameters?
+5. Browser support.
+*/
 
-  /* TODO
-  1. Better form inputs.
-  // List form types need serious cleanup / simplification
-       - Non-color forms should be list as well, row of blocks with active highlighted
-       - Move all buttons into template
-  1a. Dismiss form when user clicks off
-  2. Don't need regexes, can just use string matches
-  3. Design a bunch of special parts (with special[1-9] + class toggle + psuedo elements)
-  4. Let user save? URL parameters?
-  5. Browser support.
-  */
-
-  var formTemplate = _.template(document.getElementsByClassName('js-form-template')[0].innerHTML),
-      fieldTemplate = _.template(document.getElementsByClassName('js-field-template')[0].innerHTML),
-      formContainer = document.getElementsByClassName('js-form')[0],
-      config = {
-        'height': {
-          'regex': /h[0-9\-]+/,
-          'min':4,
-          'max':12,
-          'form': 'increment'
-        },
-        'width': {
-          'regex': /w[0-9\-]+/,
-          'min':4,
-          'max':12,
-          'form': 'increment'
-        },
-        'color': {
-          'regex': /bg[0-9\-]+/,
-          'min':1,
-          'max':17,
-          'form': 'list'
-        },
-        'radius': {
-          'regex': /r[0-9]+/,
-          'min':1,
-          'max':4,
-          'form': 'increment'
-        },
-        'rotate': {
-          'regex': /rotate[0-9\-]+/,
-          'min':-4,
-          'max':4,
-          'form': 'increment'
-        },
-        'vertical gap': {
-          'regex': /margin[b|t][0-9\-]+/,
-          'min':-4,
-          'max':4,
-          'form': 'increment'
-        },
-        'horizontal gap': {
-          'regex': /margin[l|r][0-9\-]+/,
-          'min':-4,
-          'max':4,
-          'form': 'increment'
-        }
-      };
-
-  var configurable = document.getElementsByClassName('js-configurable');
+var form = document.getElementsByClassName('js-form')[0],
+    fields = document.getElementsByClassName('js-fields');
+    configurable = document.getElementsByClassName('js-configurable'),
+    config = {
+      'height': {
+        'regex': /h[0-9\-]+/,
+        'min':4,
+        'max':12,
+        'form': 'increment'
+      },
+      'width': {
+        'regex': /w[0-9\-]+/,
+        'min':4,
+        'max':12,
+        'form': 'increment'
+      },
+      'color': {
+        'regex': /bg[0-9\-]+/,
+        'min':1,
+        'max':17,
+        'form': 'list'
+      },
+      'radius': {
+        'regex': /r[0-9]+/,
+        'min':1,
+        'max':4,
+        'form': 'increment'
+      },
+      'rotate': {
+        'regex': /rotate[0-9\-]+/,
+        'min':-4,
+        'max':4,
+        'form': 'increment'
+      },
+      'vertical gap': {
+        'regex': /margin[b|t][0-9\-]+/,
+        'min':-4,
+        'max':4,
+        'form': 'increment'
+      },
+      'horizontal gap': {
+        'regex': /margin[l|r][0-9\-]+/,
+        'min':-4,
+        'max':4,
+        'form': 'increment'
+      }
+  };
 
   /* Event handlers */
-  for (var i = 0; i < configurable.length; i++) {
+  for (var i = 0;i < configurable.length; i++) {
 
     configurable[i].addEventListener('click', function(ev) {
-      var prevForm = document.getElementsByClassName('js-fields')[0];
-      if (prevForm) {
-        removeForm(ev.currentTarget, prevForm);
+
+      if (fields) {
+        removeForm(ev.currentTarget, fields);
       } else {
         makeForm(ev.currentTarget);
       }
@@ -76,19 +69,24 @@
 
   }
 
-  /* Remove form */
-  var removeForm = function(bodyPart, prevForm) {
+  var addClick = function(ev) {
+  }
 
+  /* Remove form */
+  var removeForm = function(bodyPart) {
     var isActive = bodyPart.classList.contains('active');
 
-    prevForm.classList.remove('in');
+    for (var i = 0; i < fields.length; i++) {
+      fields[i].classList.remove('in');
+    }
 
     for (var i = 0; i < configurable.length; i++) {
       configurable[i].classList.remove('active');
     }
 
     window.setTimeout(function() {
-      formContainer.innerHTML = '';
+      form.innerHTML = '';
+      form.classList.remove('isright');
       if (!isActive) {
         makeForm(bodyPart);
       }
@@ -101,61 +99,80 @@
 
     bodyPart.classList.add('active');
 
-    formContainer.innerHTML = formTemplate({element: bodyPart.id});
-
-    var form = document.getElementsByClassName('js-fields')[0];
+    var fields = document.createElement('DIV');
+      fields.id = 'form-' + bodyPart.id;
+      fields.className = 'js-fields fields in animate1';
 
     // Create form fields
     for (var key in config) {
-      var classes = bodyPart.className;
-      var match = classes.search(config[key].regex);
+      var match = bodyPart.className.search(config[key].regex);
+      var count = config[key].max - config[key].min;
+
+      var field = document.createElement('DIV');
+        field.id = 'field-' + key;
+        field.className = 'cf field js-field';
+
+      var label = document.createElement('DIV');
+        label.className = 'block label';
+
+      var labelText = document.createTextNode(key);
+        label.appendChild(labelText);
+
+      field.appendChild(label);
+
       if (match > -1) {
-        form.innerHTML += fieldTemplate({ attribute: key });
+        fields.appendChild(field);
+
+        var currentClass = getClassFromEl(bodyPart, config[key].regex);
+
+        // Create buttons
+        for (var i = 0; i < count;i++ ) {
+          var button = document.createElement('A');
+            button.setAttribute('href','#');
+            button.dataset.attribute = key;
+            button.dataset.num = config[key].min + i;
+            button.className = 'attr-' + key + i + ' button-' + config[key].form + ' button button-list js-list js-input';
+
+            if (parseInt(button.dataset.num) === getNumberFromClass(currentClass)) {
+              button.classList.add('active');
+            }
+          field.appendChild(button);
+        }
       }
+    }
 
-      if (config[key].form === 'list') {
-        var field = document.getElementById('field-' + key);
-        var count = config[key].max - config[key].min;
+    form.appendChild(fields);
 
-        for (var i = 0;i <= count;i++ ) {
-          var buttonTemplate = "<a href='#' data-attribute='" + key + "' data-num='" + i + "' class='bg" + i + " button button-list js-list js-input'></a>";
-          field.innerHTML += buttonTemplate;
-        };
-      }
+    /* Event handlers for our new buttons */
+    var buttons = document.getElementsByClassName('js-input');
 
-      /* Event handlers for our new inputs */
-      var inputs = document.getElementsByClassName('js-input');
-      for (var i = 0; i < inputs.length; i++) {
-        inputs[i].addEventListener('click', function(ev, key) {
-          var target = ev.currentTarget;
-          var increment = target.classList.contains('js-up') ? 1 : -1;
-          var newNum = target.classList.contains('js-list') ? target.dataset.num : null;
-          var attribute = target.dataset.attribute;
-
-          changeClass(bodyPart, attribute, increment, newNum);
-          ev.stopImmediatePropagation();
-        });
-      }
-    };
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', function(ev) {
+        var target = ev.currentTarget;
+        var attribute = target.dataset.attribute;
+        var newNum = target.dataset.num;
+        changeClass(bodyPart, attribute, newNum);
+        ev.stopImmediatePropagation();
+      });
+    }
 
     /* Form positioning */
     var topSpace = Math.floor(bodyPart.getBoundingClientRect().top);
     var rightSpace = Math.floor(bodyPart.getBoundingClientRect().right);
+    var formHeight = Math.floor(form.offsetHeight);
+    var partHeight = Math.floor(bodyPart.offsetHeight);
     var formWidth = Math.floor(form.offsetWidth);
     var isTop = topSpace < window.innerHeight / 2;
     var isRight = rightSpace < window.innerWidth / 2;
 
     window.requestAnimationFrame(function() {
-      form.classList.add('in');
-      if (isTop) form.classList.add('istop');
       if (isRight) form.classList.add('isright');
     });
 
-    form.style.top = isTop ? topSpace + 'px' : 'auto';
-    form.style.bottom = !isTop ? window.innerHeight - (topSpace + bodyPart.offsetHeight) + 'px' : 'auto';
-    form.style.left = !isRight ? rightSpace + 40 + 'px' : rightSpace - (formWidth + bodyPart.offsetWidth) - 40  + 'px';
+    form.style.bottom = !isTop ? window.innerHeight - (topSpace + partHeight) - 40 + 'px' : window.innerHeight - (formHeight + topSpace) + 40 + 'px';
+    form.style.left = !isRight ? rightSpace + 40 + 'px' : rightSpace - (formWidth + partHeight) - 20  + 'px';
 
-  };
+  }
 
   /* Manipulate attributes */
   var getClassFromEl = function(el, classRegex) {
@@ -167,19 +184,10 @@
     return parseInt(className.substr(index));
   };
 
-  var changeClass = function(el, attribute, increment, newNum) {
+  var changeClass = function(el, attribute, newNum) {
+
     var currentClass = getClassFromEl(el,config[attribute].regex);
     var currentNum = getNumberFromClass(currentClass);
-    var newNum = newNum || currentNum + increment;
-
-    if (increment === 1 && currentNum >= config[attribute].max) {
-      newNum = config[attribute].min;
-    }
-
-    if (increment === -1 && currentNum <= config[attribute].min) {
-      newNum = config[attribute].max;
-    }
-
     var newClass = currentClass.replace(currentNum,newNum);
 
     el.classList.remove(currentClass);
@@ -187,7 +195,6 @@
 
     /* Match leg heights to each other */
     var bodyPartName = el.id;
-
     if (bodyPartName.indexOf('leg') > -1 && attribute === 'height') {
       var pair = 'leg-' +
         (bodyPartName.indexOf('lower') > -1 ? 'lower-' : 'upper-') +
