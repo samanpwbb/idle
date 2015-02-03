042b509aef4ea040d1f588999a34728b04ff8806
samanpwbb <samanpwbb@gmail.com>
2015-02-03 09:27:43 -0500

    start cleanup

--- site.js
+++ site.js
@@ -1,93 +1,95 @@
 /* TODO
-1. Design a bunch of special parts (with special[1-x] + class toggle + psuedo elements)
-2. Let user save? Keep track of all data in URL? (this will be interesting / hard)
+1. Let user save? Keep track of all data in URL? (this will be interesting / hard)
+2. Highlight limb matches
 3. More interesting form elements, direct select up/down/left/right
-4. Browser support.
+4. Move margin x/y to direct select.
+5. Browser support.
 */
 
 var background = document.getElementsByClassName('js-background')[0],
-    form = document.getElementsByClassName('js-form')[0],
-    fields = document.getElementsByClassName('js-fields'),
-    buttons = document.getElementsByClassName('js-input');
-    configurable = document.getElementsByClassName('js-configurable'),
-    config = {
-      'special': {
-        'min':0,
-        'max':2,
-        'form': 'names',
-        'names': {
-          'eyes': [
-            'none',
-            'cyclops',
-            'eyes'
-          ],
-          'head': [
-            'none',
-            'ears',
-            'antennae'
-          ],
-          'leg-lower-right': [
-            'none',
-            'foot',
-            'details'
-          ],
-          'leg-lower-left': [
-            'none',
-            'foot',
-            'details'
-          ],
-          'arm-upper-left': [
-            'none',
-            'details',
-            'wings'
-          ],
-          'arm-upper-right': [
-            'none',
-            'details',
-            'wings'
-          ]
-        }
-      },
-      'background': {
-        'min':0,
-        'max':17,
-        'form': 'list'
-      },
-      'height': {
-        'min':4,
-        'max':12,
-        'form': 'increment'
-      },
-      'width': {
-        'min':4,
-        'max':12,
-        'form': 'increment'
-      },
-      'radius': {
-        'min':1,
-        'max':4,
-        'form': 'increment'
-      },
-      'vertical gap': {
-        'min':-4,
-        'max':4,
-        'form': 'increment'
-      },
-      'horizontal gap': {
-        'min':-4,
-        'max':4,
-        'form': 'increment'
+  form = document.getElementsByClassName('js-form')[0],
+  fields = document.getElementsByClassName('js-fields'),
+  buttons = document.getElementsByClassName('js-input');
+  configurable = document.getElementsByClassName('js-configurable'),
+  storedBody = {},
+  config = {
+    'special': {
+      'min':0,
+      'max':2,
+      'form': 'names',
+      'names': {
+        'eyes': [
+          'none',
+          'cyclops',
+          'eyes'
+        ],
+        'head': [
+          'none',
+          'ears',
+          'antennae'
+        ],
+        'leg-lower-right': [
+          'none',
+          'foot',
+          'details'
+        ],
+        'leg-lower-left': [
+          'none',
+          'foot',
+          'details'
+        ],
+        'arm-upper-left': [
+          'none',
+          'details',
+          'wings'
+        ],
+        'arm-upper-right': [
+          'none',
+          'details',
+          'wings'
+        ]
       }
+    },
+    'background': {
+      'min':0,
+      'max':17,
+      'form': 'list'
+    },
+    'height': {
+      'min':4,
+      'max':12,
+      'form': 'increment'
+    },
+    'width': {
+      'min':4,
+      'max':12,
+      'form': 'increment'
+    },
+    'radius': {
+      'min':1,
+      'max':4,
+      'form': 'increment'
+    },
+    'marginy': {
+      'min':-4,
+      'max':4,
+      'form': 'increment'
+    },
+    'marginx': {
+      'min':-4,
+      'max':4,
+      'form': 'increment'
+    }
   };
 
-  /* Event handlers */
+  /* UI Basics */
   var timer = null;
   window.onresize = function() {
     if (timer != null) clearTimeout(timer);
 
     timer = setTimeout(function() {
       var activePart = document.getElementsByClassName('js-configurable active')[0] || false;
-      handleInput(activePart, true);
+      positionForm(activePart);
     }, 100);
   };
 
@@ -115,10 +117,9 @@ var background = document.getElementsByClassName('js-background')[0],
       handleInput(ev.currentTarget);
       ev.stopImmediatePropagation();
     });
-
   }
 
-  var handleInput = function(target, refresh) {
+  var handleInput = function(target) {
     var isActive = target ? target.classList.contains('active') : false;
     var isBodyPart = target.classList.contains('js-configurable');
 
@@ -128,7 +129,7 @@ var background = document.getElementsByClassName('js-background')[0],
 
     form.innerHTML = '';
 
-    if (!isActive && isBodyPart || refresh && isBodyPart) {
+    if (!isActive && isBodyPart) {
       makeForm(target);
     } else {
       form.style.bottom = null;
@@ -146,7 +147,7 @@ var background = document.getElementsByClassName('js-background')[0],
 
     // Create form fields
     for (var key in config) {
-      var match = bodyPart.className.search(config[key].regex);
+      var match = bodyPart.className.search(key);
       var count = (config[key].max - config[key].min) + 1;
 
       var field = document.createElement('DIV');
@@ -172,7 +173,7 @@ var background = document.getElementsByClassName('js-background')[0],
           var button = document.createElement('A');
             button.setAttribute('href','#');
             button.dataset.attribute = key;
-            button.dataset.num = config[key].min + i;
+            button.dataset.newClass = key + config[key].min + i;
             button.className = 'attr-' + key + i + ' button-' + config[key].form + ' button js-list js-input';
             if (config[key].form === 'names') {
               var nameList = config[key].names[bodyPart.id];
@@ -192,27 +193,29 @@ var background = document.getElementsByClassName('js-background')[0],
     for (var i = 0; i < buttons.length; i++) {
       buttons[i].addEventListener('click', function(ev) {
         var target = ev.currentTarget;
-        var attribute = target.dataset.attribute;
-        var newClass = currentClass.replace(currentNum,target.dataset.num);
-        // this is broken
-        setBodyPartClass(bodyPart, attribute, newClass);
+        // this should update the data model and then the data
+        // model should update the dom
+        updateData(bodyPart, target.dataset.attribute, target.dataset.newClass);
         ev.stopImmediatePropagation();
-
       });
     }
 
-    /* Form positioning */
-    var topSpace = Math.floor(bodyPart.getBoundingClientRect().top);
-    var rightSpace = Math.floor(bodyPart.getBoundingClientRect().right);
+    positionForm(bodyPart);
+
+  };
+
+  /* Form positioning */
+  var positionForm = function(el) {
+    var topSpace = Math.floor(el.getBoundingClientRect().top);
+    var rightSpace = Math.floor(el.getBoundingClientRect().right);
     var formHeight = Math.floor(form.offsetHeight);
-    var partHeight = Math.floor(bodyPart.offsetHeight);
+    var partHeight = Math.floor(el.offsetHeight);
     var formWidth = Math.floor(form.offsetWidth);
     var isTop = topSpace < window.innerHeight / 2;
     var isRight = rightSpace < window.innerWidth / 2;
 
     form.style.bottom = !isTop ? window.innerHeight - (topSpace + partHeight) - 40 + 'px' : window.innerHeight - (formHeight + topSpace) + 40 + 'px';
     form.style.left = !isRight ? rightSpace + 40 + 'px' : rightSpace - (formWidth + partHeight) - 20  + 'px';
-
   }
 
   /* Manipulate attributes */
@@ -230,9 +233,10 @@ var background = document.getElementsByClassName('js-background')[0],
     return parseInt(className.substr(index));
   };
 
+  /* This needs to be based on data model */
   var setActiveClass = function(button, bodyPart) {
     var attribute = button.dataset.attribute,
-      currentClass = getClassFromEl(bodyPart, config[attribute].regex);
+      currentClass = getClassFromEl(bodyPart, attribute);
 
     for (var i = 0; i < buttons.length; i++) {
       if (buttons[i].classList.contains('active') && buttons[i].dataset.attribute === attribute) {
@@ -246,38 +250,31 @@ var background = document.getElementsByClassName('js-background')[0],
 
   };
 
-  /* this needs to be reworked to only take bodyPart and newClass arguments,
-  target should adapt based on changes to data model */
   var setBodyPartClass = function(el, attribute, newClass) {
-    var currentClass = getClassFromEl(el,attribute),
-      currentNum = getNumberFromClass(currentClass),
-
+    var currentClass = getClassFromEl(el,attribute);
     el.classList.remove(currentClass);
     el.classList.add(newClass);
 
-    /* this needs to be changed to be based on data model */
-    setActiveClass(target, el);
-
     /* Match leg heights to each other */
-    var bodyPartName = el.id;
-    if (bodyPartName.indexOf('leg') > -1) {
+    var name = el.id;
+    if (name.indexOf('leg') > -1) {
       var pair = 'leg-' +
-        (bodyPartName.indexOf('lower') > -1 ? 'lower-' : 'upper-') +
-        (bodyPartName.indexOf('left') > -1 ? 'right' : 'left');
+        (name.indexOf('lower') > -1 ? 'lower-' : 'upper-') +
+        (name.indexOf('left') > -1 ? 'right' : 'left');
       document.getElementById(pair).classList.remove(currentClass);
       document.getElementById(pair).classList.add(newClass);
     };
 
-    if (bodyPartName.indexOf('arm') > -1) {
+    if (name.indexOf('arm') > -1) {
       var pair = 'arm-' +
-        (bodyPartName.indexOf('lower') > -1 ? 'lower-' : 'upper-') +
-        (bodyPartName.indexOf('left') > -1 ? 'right' : 'left');
+        (name.indexOf('lower') > -1 ? 'lower-' : 'upper-') +
+        (name.indexOf('left') > -1 ? 'right' : 'left');
       document.getElementById(pair).classList.remove(currentClass);
       document.getElementById(pair).classList.add(newClass);
     };
 
     /* Exception for chest - this is ugly */
-    if (bodyPartName === 'chest' && attribute === ('width' || 'vertical gap')) {
+    if (name === 'chest' && attribute === ('width' || 'vertical gap')) {
       var bodyContainer = document.getElementById('body-container');
       bodyContainer.classList.remove(currentClass);
       bodyContainer.classList.add(newClass);
@@ -286,41 +283,53 @@ var background = document.getElementsByClassName('js-background')[0],
     return false;
   };
 
-  /* Data model */
-  var storedBody = {};
-  var getDataFromClasses = function() {
+/* Data model */
+var getDatafromElements = function(els) {
 
-    for (var i = 0; i < configurable.length; i++) {
+  for (var i = 0; i < els.length; i++) {
+    var el = els[i];
 
-      var el = configurable[i];
+    /* Add body parts as keys */
+    storedBody[el.id] = [];
 
-      /* Grab each body part, add as keys */
-      storedBody[el.id] = [];
-
-      /* Grab all modifiable classnames and add them to corresponding keys */
-      for (var key in config) {
-        var match = configurable[i].className.search(key);
-        if (match > -1) {
-          var part = getClassFromEl(el, key);
-          storedBody[configurable[i].id].push(part);
-        }
-      }
-      /*
-      Now we should have a data object that can be passed into setBodyPartClass
-      We will need to:
-        1. Click handlers update this object
-        2. After click, updated object is passed to render function
-      */
-    }
-  };
-
-  getDataFromClasses();
-
-  var addClassesfromData = function() {
-    for (key in storedBody) {
-      for (i in storedBody[key]) {
-        var bodyPart = document.getElementById(key);
-        setBodyPartClass(bodyPart, storedBody[key][i]);
+    /* Grab all modifiable classnames and add them to corresponding keys */
+    for (var key in config) {
+      var match = els[i].className.search(key);
+      if (match > -1) {
+        var part = getClassFromEl(el, key);
+        storedBody[els[i].id].push(part);
       }
     }
-  };
+  }
+};
+
+var updateData = function(els) {
+  var oldStoredBody = storedBody;
+  if (oldStoredBody !== storedBody) {
+    addClassesfromData(storedBody);
+  }
+};
+
+var addClassesfromData = function(data) {
+  for (key in data) {
+    for (var i = 0; i < data[key].length; i++) {
+      var el = document.getElementById(key);
+      setBodyPartClass(el, key, data[key][i]);
+    }
+  }
+};
+
+var initialize = function() {
+  storedBody = getDatafromElements(configurable);
+
+  // Need to actually save data to params
+  var params = false;
+
+  if (params) {
+    addClassesfromData(storedBody);
+  }
+
+};
+
+initialize();
+
