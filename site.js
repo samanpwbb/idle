496b72aa643c706475e559e553aa7599b8b8e07e
samanpwbb <samanpwbb@gmail.com>
2015-02-09 00:29:36 -0500

    fix bitly api request

--- site.js
+++ site.js
@@ -400,7 +400,7 @@ function getShortUrl() {
   request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
       // Success!
-      var data = JSON.parse(request.responseText);
+      var data = JSON.parse(request.responseText).data;
       renderShortUrl(data.url);
     } else {
       // We reached our target server, but it returned an error
